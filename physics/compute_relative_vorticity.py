dfac86231ad893e84ac4675e0a8e1a04fa522fc5
Thomas Wilder <tmwilder@outlook.com>
2026-01-19 11:20:17 +0000

    made changes to how load in data rom subset velocity data

--- physics/compute_relative_vorticity.py
+++ physics/compute_relative_vorticity.py
@@ -8,21 +8,30 @@
 
 import glob
 import xarray as xr
-from xnemogcm import open_nemo_and_domain_cfg, get_metrics
+from xnemogcm import open_domain_cfg, get_metrics
 import xgcm
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import numpy as np
 
+import logging
 
-directory = '/gws/nopw/j04/ai4pex/twilder/NEMO_data/DINO/EXP16/features/'
-mask_path = ['~/Python/AI4PEX/DINO/mesh_mask_exp16.nc']
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='compute_vorticity.log', level=logging.INFO, filemode='w')
+
+logger.info('Begin...')
+
+region = 'SO_JET'
+
+
+directory = f'/gws/nopw/j04/ai4pex/twilder/NEMO_data/DINO/EXP16/features/{region}/'
+mask_path = [directory + f'mesh_mask_exp16_{region}.nc']
 
 # Initial date string
 start_date_init_str = "00610101"
 
 # End date string
-end_date_init_str = "00730101"
+end_date_init_str = "00610201"
 
 
 # Convert date strings to datetime objects
@@ -32,6 +41,8 @@ end_date_init = datetime.strptime(end_date_init_str, "%Y%m%d")
 # Loop to increment date by one day
 current_date_init = start_date_init
 while current_date_init < end_date_init:
+
+    logger.info('Processing date: %s', current_date_init)
 
     # Increment for next date
     next_date_init = current_date_init + relativedelta(months=+1)
@@ -51,8 +62,8 @@ while current_date_init < end_date_init:
     current_date_init = next_date_init
 
     # set nemo filename using dates
-    nemo_files = [f'MINT_1d_{date_init}_*_ug.nc',
-                  f'MINT_1d_{date_init}_*_vg.nc']
+    nemo_files = [f'MINT_1d_{date_init}_*_ug_{region}.nc',
+                  f'MINT_1d_{date_init}_*_vg_{region}.nc']
     print(nemo_files)
 
     nemo_paths = [glob.glob(directory + f) for f in nemo_files]
@@ -61,8 +72,18 @@ while current_date_init < end_date_init:
 
     # open dataset using xnemogcm
     nemo_paths = [directory + f for f in nemo_files]
-    ds = open_nemo_and_domain_cfg(nemo_files=nemo_paths,
-                                  domcfg_files=mask_path)
+    # ds = open_nemo_and_domain_cfg(nemo_files=nemo_paths,
+    #                               domcfg_files=mask_path)
+    #! issues opening subsetted data with full xnemogcm, just hangs?
+    #* use this workaround to open data and domcfg separately
+    dataU = xr.open_dataset(nemo_paths[0])
+    dataV = xr.open_dataset(nemo_paths[1])
+    domcfg = open_domain_cfg(files = mask_path)
+
+    # get surface domcfg
+    domcfg_surface = domcfg.isel(z_c=0, z_f=0)
+
+    # logger.info('Dataset loaded is: %s', ds)
 
     # set up xgcm grid
     # grid = xgcm.Grid(
@@ -72,39 +93,44 @@ while current_date_init < end_date_init:
     #             boundary={'Y': 'extend'},
     #             )
 
-    grid = xgcm.Grid(
-                ds,
-                metrics=get_metrics(ds),
-    )
+    grid = xgcm.Grid(domcfg, 
+                     metrics=get_metrics(domcfg))
+    
+    bd = {'boundary': 'extend'}
 
     # subset data for surface
-    ds_ss = ds.isel(z_c=0, z_f=0)
+    # ds_ss = ds.isel(z_c=0, z_f=0)
+
+    # logger.info('Dataset subsetted to surface is: %s', ds_ss)
 
     # remove ds from memory
-    del ds
+    # del ds
 
     # compute relative vorticity
-    zeta = 1/(ds_ss.e1f*ds_ss.e2f) * \
-           ( grid.diff(ds_ss.vg*ds_ss.e2v, 'X', 
-                       boundary='fill', fill_value=0) \
-           - grid.diff(ds_ss.ug*ds_ss.e1u, 'Y', 
-                       boundary='fill', fill_value=0) ) * ds_ss.fmask
+    zeta = 1/(domcfg_surface.e1f*domcfg_surface.e2f) * \
+            (grid.diff(dataV.vg*domcfg_surface.e2v, 'X', **bd) \
+             - grid.diff(dataU.ug*domcfg_surface.e1u, 'Y', **bd)) \
+                * domcfg_surface.fmask
+    
+    logger.info('Relative vorticity calculated is: %s', zeta)
 
     # zero over equator region
-    zeta = xr.where(((ds_ss.gphif>2) | (ds_ss.gphif<-2)), zeta, 0.0)
+    # if region == 'full':
+    #     zeta = xr.where(((ds_ss.gphif>2) | (ds_ss.gphif<-2)), zeta, 0.0)
 
     # create dataset
     ds_tmp = xr.Dataset(
         data_vars={
-            'vor': (["y", "x", "time_counter"], zeta.values),
+            'vor': (["y_f", "x_f", "t"], 
+                        zeta.values),
         },
         coords={
-            "time_counter": (["time_counter"], ds_ss.t.values,
-                             ds_ss.t.attrs),
-            "gphif": (["y", "x"], ds_ss.gphif.values,
+            "t": (["t"], zeta.t.values,
+                        zeta.t.attrs),
+            "gphif": (["y_f", "x_f"], domcfg_surface.gphif.values, 
                       {"standard_name": "Latitude", "units": "degrees_north"}),
-            "glamf": (["y", "x"], ds_ss.glamf.values,
-                      {"standard_name": "Longitude", "units": "degrees_east"}),
+            "glamf": (["y_f", "x_f"], domcfg_surface.glamf.values, 
+                      {"standard_name": "Longitude","units": "degrees_east"}),
         },
         attrs={
             "name": "NEMO dataset",
@@ -113,6 +139,8 @@ while current_date_init < end_date_init:
         },
     )
 
+    logger.info('Dataset to be saved is: %s', ds_tmp)
+
     # extract date_end from nemo_paths
     filename = nemo_paths[0].split('/')[-1]
     date_end = filename.split('_')[3]
@@ -120,14 +148,14 @@ while current_date_init < end_date_init:
 
     # extract time counter bounds from original file
     ref = xr.open_dataset(directory +
-                          f'MINT_1d_{date_init}_{date_end}_ug.nc')
+                          f'MINT_1d_{date_init}_{date_end}_ug_{region}.nc')
 
     ds_tmp["time_counter_bounds"] = ref["time_counter_bounds"]
-    ds_tmp["time_counter_bounds"] = ref["time_counter_bounds"]
+    # ds_tmp["time_counter_bounds"] = ref["time_counter_bounds"]
 
     # save data to netcdf
-    output_file = f'MINT_1d_{date_init}_{date_end}_vor.nc'
+    output_file = f'MINT_1d_{date_init}_{date_end}_vor_{region}.nc'
 
-    save_directory = '/gws/nopw/j04/ai4pex/twilder/NEMO_data/DINO/EXP16/features/'
+    save_directory = f'/gws/nopw/j04/ai4pex/twilder/NEMO_data/DINO/EXP16/features/{region}/'
 
     ds_tmp.to_netcdf(save_directory + output_file)
