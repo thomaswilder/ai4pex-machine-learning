d96c0ab8e5f2f6a554b326c5f82e7c85a7e4e03b
Thomas Wilder <tmwilder@outlook.com>
2026-01-19 14:29:03 +0000

    Set grid T to x and y coordinates, hack for not computing Ld with x_c and y_c xnemo type coordinates

--- misc/subset_dino.py
+++ misc/subset_dino.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
 '''
-Description: This script will subset DINO EXP16 velocity and deformation radius data.
-    Doing so will speed up the remaining computations for vorticity, MKE and EKE.
+Description: This script will subset DINO EXP16 velocity and bn2 data.
+    Doing so will speed up the remaining computations for vorticity, 
+    Ld, MKE and EKE.
 
 Method: 
     Use EXP4 mask file to confirm if regridded data set will be of an adequate size.
+    Uses indices to subset, which are found in subset_dino.ipynb
+
+
 
 '''
 
@@ -15,17 +19,29 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 #------------ Parameters to set ------------#
-variable = 'ug'
+variable = 'Ld'
+
+grid = 'T'
 
 region = 'SO_JET'
 
 open_directory = '/gws/nopw/j04/ai4pex/twilder/NEMO_data/DINO/EXP16/features/'
 save_directory = f'/gws/nopw/j04/ai4pex/twilder/NEMO_data/DINO/EXP16/features/{region}/'
 
-lat_min = -47.5
-lat_max = -37.5
-lon_min = 2.5
-lon_max = 17.5
+#* dictionary listing regions and indices
+regions = {
+    'SO_JET': {'jmin': 726,
+               'jmax': 942,
+               'imin': 41,
+               'imax': 279},
+}
+
+region_params = regions[region]
+jmin = region_params['jmin']
+jmax = region_params['jmax']
+imin = region_params['imin']
+imax = region_params['imax']
+
 #------------------------------------------#
 
 # Initial date string
@@ -68,13 +84,33 @@ while current_date_init < end_date_init:
     ds = xr.open_dataset(nemo_paths[0][0])
 
     # mask and subset data 
-    ds_masked = ds.where( (lat_min < ds.gphiu) &
-            (ds.gphiu < lat_max) &
-            (lon_min < ds.glamu) &
-            (ds.glamu < lon_max)
-            )
+    if grid == 'U':
+        ds_masked = ds.where( (ds.gphiu.isel(x_f=imin-1,y_c=jmin-1).values < ds.gphiu) &
+                (ds.gphiu < ds.gphiu.isel(x_f=imax,y_c=jmax).values) &
+                (ds.glamu.isel(x_f=imin-1,y_c=jmin-1).values < ds.glamu) &
+                (ds.glamu < ds.glamu.isel(x_f=imax,y_c=jmax).values)
+                )
 
-    ds_subset = ds_masked.dropna(dim='y', how='all').dropna(dim='x', how='all')
+        ds_subset = ds_masked.dropna(dim='y_c', how='all').dropna(dim='x_f', how='all')
+    elif grid == 'V':
+        ds_masked = ds.where( (ds.gphiv.isel(x_c=imin-1,y_f=jmin-1).values < ds.gphiv) &
+                (ds.gphiv < ds.gphiv.isel(x_c=imax,y_f=jmax).values) &
+                (ds.glamv.isel(x_c=imin-1,y_f=jmin-1).values < ds.glamv) &
+                (ds.glamv < ds.glamv.isel(x_c=imax,y_f=jmax).values)
+                )
+
+        ds_subset = ds_masked.dropna(dim='y_f', how='all').dropna(dim='x_c', how='all')
+    #* note indice change as Ld was computed and set with xnemo readable
+    elif grid == 'T':
+        ds_masked = ds.where( (ds.gphit.isel(x=imin-1,y=jmin-1).values < ds.gphit) &
+                (ds.gphit < ds.gphit.isel(x=imax,y=jmax).values) &
+                (ds.glamt.isel(x=imin-1,y=jmin-1).values < ds.glamt) &
+                (ds.glamt < ds.glamt.isel(x=imax,y=jmax).values)
+                )
+
+        ds_subset = ds_masked.dropna(dim='y', how='all').dropna(dim='x', how='all')
+    else:
+        print('Grid type not recognised. Please use U, V or T.')
 
     # extract date_end from filename
     filename = nemo_paths[0][0].split('/')[-1]
