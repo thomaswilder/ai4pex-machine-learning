03cf12ca3d0e1d33c1bd64548bb9b1916043e195
Thomas Wilder <tmwilder@outlook.com>
2026-01-20 13:50:33 +0000

    added some logic to filter eke

--- filtering/filter_input_features.py
+++ filtering/filter_input_features.py
@@ -9,7 +9,7 @@
 
 import gcm_filters
 import xarray as xr
-from xnemogcm import open_nemo_and_domain_cfg, get_metrics
+from xnemogcm import open_domain_cfg, get_metrics
 import xgcm
 import numpy as np
 from datetime import datetime
@@ -25,11 +25,17 @@ logger.info('Begin...')
 
 # ------------ misc parameters ---------------- #
 
-directory = '/gws/nopw/j04/ai4pex/twilder/NEMO_data/DINO/EXP16/features/'
-mask16_path = ['~/Python/AI4PEX/DINO/mesh_mask_exp16.nc']
-mask025_path = '/gws/nopw/j04/ai4pex/twilder/NEMO_data/DINO/EXP4/mesh_mask_025.nc'
+directory = '/gws/nopw/j04/ai4pex/twilder/NEMO_data/DINO/EXP16/features/SO_JET/'
+mask16_path = [directory + 'mesh_mask_exp16_SO_JET.nc']
+mask025_path = directory + 'mesh_mask_exp4_SO_JET.nc'
 
-variable = 'vor'
+region = 'SO_JET'
+
+variable = 'ke'
+variable_to_filter = 'fine_ke'
+variable_name = 'eke'
+
+#TODO add xnemo option to load in deformation radius
 
 # open spatial filter
 kappa = xr.open_dataset('/gws/nopw/j04/ai4pex/twilder/NEMO_data/DINO/EXP16/kappa16.nc')
@@ -77,14 +83,25 @@ while current_date_init < end_date_init:
     current_date_init = next_date_init
 
     # set nemo filename using dates
-    nemo_files = [f'MINT_1d_{date_init}_*_grid_{variable}.nc']
+    nemo_files = [f'MINT_1d_{date_init}_*_{variable}_{region}.nc']
     print(nemo_files)
 
     # open dataset using xnemogcm
     # nemo_paths = [directory + f for f in nemo_files]
     nemo_paths =  [glob.glob(directory + f) for f in nemo_files]
-    ds = open_nemo_and_domain_cfg(nemo_files=nemo_paths[0],
-                                  domcfg_files=mask16_path)
+    # ds = open_nemo_and_domain_cfg(nemo_files=nemo_paths[0],
+    #                               domcfg_files=mask16_path)
+
+    domcfg = open_domain_cfg(files = mask16_path)
+    data = xr.open_dataset(nemo_paths[0][0])
+
+    if variable == 'Ld':
+        data = data.rename({'time_counter': 't', 
+                            'x': 'x_c',
+                            'y': 'y_c'})
+
+    # merge data and domcfg
+    ds = xr.merge([data, domcfg])
 
     # Rechunk the dataset
     ds=ds.chunk(dict(y_c=-1))
@@ -130,15 +147,66 @@ while current_date_init < end_date_init:
 
     # set up xgcm grid
     grid = xgcm.Grid(
-                ds,
-                metrics=get_metrics(ds),
-                periodic={'X': True, 'Y': False},
-                boundary={'Y': 'extend'},
+                domcfg,
+                metrics=get_metrics(domcfg),
                 )
+
+    bd = {'boundary': 'extend'}
     
     # interpolate if not gridT and filter
-    ds_tmp = xr.Dataset()
-    if variable in ['vor']:
-        ds['vor_c'] = grid.interp(ds.vor, ['X', 'Y'])
-        ds_tmp['vor_f'] = filter_irregular_with_land.apply(ds['vor_c'], 
+    if variable == 'vor':
+        ds['vor_c'] = grid.interp(ds.vor, ['X', 'Y'], **bd)
+        ds['vor_f'] = filter_irregular_with_land.apply(ds['vor_c'], 
                                                            dims=['y_c', 'x_c'])
+    elif variable == 'ke':
+        if variable_to_filter == 'coarse_ke':
+            ds['mke_f'] = filter_irregular_with_land.apply(
+                ds['coarse_ke'],                                            
+                dims=['y_c', 'x_c'])
+            
+        elif variable_to_filter == 'fine_ke':
+            ds['eke_f'] = filter_irregular_with_land.apply(
+                ds['fine_ke'],                                            
+                dims=['y_c', 'x_c'])
+        # variable = 'mke'
+    else:
+        ds[f'{variable}_f'] = filter_irregular_with_land.apply(ds[variable], 
+                                                           dims=['y_c', 'x_c'])
+        
+    # create dataset
+    ds_tmp = xr.Dataset(
+        data_vars={
+            f'{variable_name}': (["t", "y_c", "x_c"], 
+                        ds[f'{variable_name}_f'].values),
+        },
+        coords={
+            "t": (["t"], ds[f'{variable_name}_f'].t.values,
+                        ds[f'{variable_name}_f'].t.attrs),
+            "gphit": (["y_c", "x_c"], ds[f'{variable_name}_f'].gphit.values, 
+                      {"standard_name": "Latitude", "units": "degrees_north"}),
+            "glamt": (["y_c", "x_c"], ds[f'{variable_name}_f'].glamt.values, 
+                      {"standard_name": "Longitude","units": "degrees_east"}),
+        },
+        attrs={
+            "name": "NEMO dataset",
+            "description": f"Filtered {variable_name} at cell centre \
+                              -> ocean T grid variables",
+        },
+    )
+
+    # extract date_end from nemo_paths
+    filename = nemo_paths[0][0].split('/')[-1]
+    date_end = filename.split('_')[3]
+
+    # extract time counter bounds from original file
+    ref = xr.open_dataset(directory +
+                          f'MINT_1d_{date_init}_{date_end}_{variable}_{region}.nc')
+
+    ds_tmp["time_counter_bounds"] = ref["time_counter_bounds"]
+
+    # save data to netcdf
+    output_file = f'MINT_1d_{date_init}_{date_end}_{variable_name}_f_{region}.nc'
+
+    save_directory = f'/gws/nopw/j04/ai4pex/twilder/NEMO_data/DINO/EXP16/features/{region}/filtered_data/'
+
+    ds_tmp.to_netcdf(save_directory + output_file)
