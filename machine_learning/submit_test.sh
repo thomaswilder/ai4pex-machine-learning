232ce85de39ab383b0e28166a7e4a549d4348e58
Thomas Wilder <tmwilder@outlook.com>
2026-02-02 20:55:59 +0000

    first steps to implementing a yml config file for tep_cnn.py

---
+++ machine_learning/submit_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --partition=debug
+#SBATCH --job-name=yml_test
+#SBATCH -o %j.out
+#SBATCH -e %j.err
+#SBATCH --time=00:10:00
+#SBATCH --mem=1G
+#SBATCH --account=ai4pex
+#SBATCH --qos=debug
+
+export PATH="/home/users/twilder/Python/AI4PEX/tensorflow_env/bin:$PATH"
+
+python yml_test.py --config ./config_cnn.yml
