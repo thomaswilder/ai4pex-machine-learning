0bff984947d74574e94edfb5e770d72a7ce3d421
thomaswilder <tmwilder@outlook.com>
2026-02-11 15:21:22 +0000

    parsing_args works in main script run_cnn.py

--- machine_learning/parsing_args.py
+++ machine_learning/parsing_args.py
@@ -3,7 +3,7 @@
 import argparse
 import yaml
 
-def parse_args():
+def parse_args(argv=None):
 
     parser = argparse.ArgumentParser(description="Train a NN model on preprocessed data.")
 
@@ -33,58 +33,70 @@ def parse_args():
     parser.add_argument("--padding", type=str, default=None, help="Convolutional padding.")
     parser.add_argument("--filters", type=str, default=None, help="Number of filters in the convolutional layers.")
     parser.add_argument("--batch_size", type=int, default=None, help="Batch size for training.")
-    parser.add_argument("--k_folds", type=int, default=None, help="Number of folds for k-fold cross validation (1 = no CV).")  
-    parser.add_argument("--learning_rate", type=float, default=None, help="Learning rate for the optimizer. \
+    parser.add_argument("--k_folds", type=int, default=1, help="Number of folds for k-fold cross validation (1 = no CV).")  
+    parser.add_argument("--learning_rate", type=float, default=0.001, help="Learning rate for the optimizer. \
                         If --use_learning_rate_scheduler is set, this is the initial learning rate.")
-    parser.add_argument("--use_learning_rate_scheduler", action="store_true", \
+    parser.add_argument("--use_learning_rate_scheduler", default=None, \
                             help="Flag to indicate whether to use a learning rate scheduler. \
                             This is exponential decay.") 
     parser.add_argument("--dropout_rate", type=float, default=None, help="Dropout rate for the dropout layers.")
 
     # key flags associated with training, evaluation, prediction
-    parser.add_argument("--train", action="store_true", help="Flag to indicate whether to train the model.")
+    parser.add_argument("--train", default=None, help="Flag to indicate whether to train the model.")
     # parser.add_argument("--evaluate", action="store_true", help="Flag to indicate whether to evaluate the model.")
     # parser.add_argument("--predict", action="store_true", help="Flag to indicate whether to predict using the model.")
 
     #TODO pickup will not work when using learning rate scheduler. Adapt this. ?
-    parser.add_argument("--pickup", action="store_true", help="Flag to indicate whether to pick up training.")
-    parser.add_argument("--verbose", action="store_true", help="Verbose output during training.")
-    parser.add_argument("--local_norm", action="store_true", help="Normalize training data within local domain.")
-    parser.add_argument("--global_norm", action="store_true", help="Normalize training data over all domains.")
+    parser.add_argument("--pickup", default=None, help="Flag to indicate whether to pick up training.")
+    parser.add_argument("--verbose", default=None, help="Verbose output during training.")
+    parser.add_argument("--local_norm", default=None, help="Normalize training data within local domain.")
+    parser.add_argument("--global_norm", default=None, help="Normalize training data over all domains.")
     
     # logging and monitoring
     # # parser.add_argument("--tensboard_log_dir", type=str, default=None, help="Directory to save TensorBoard logs.")
     parser.add_argument("--tensboard_log_dir", type=str, default=None, help="Name of the TensorBoard log directory.")
     # # parser.add_argument("--verbose_output_dir", type=str, default=None, help="Directory to save verbose output logs.")
     parser.add_argument("--verbose_output_filename", type=str, default=None, help="Name of the verbose output log file.")
+ 
+    # arguments that come in via the sbatch script
+    pre_args, remaining = parser.parse_known_args(argv)
 
-    pre_args, remaining = parser.parse_known_args()
+    # print("Pre args:", pre_args)
+    # print("Remaining args:", remaining)
 
-    print("Pre args:", pre_args)
-    print("Remaining args:", remaining)
-
+    # load in arguments from yaml config file
     cfg = {}
     if pre_args.config:
         cfg = yaml.safe_load(open(pre_args.config, 'r'))
-
+        
         mode = cfg.pop("mode")
         if mode == "train":
             cfg["train"] = True
         else:
             raise ValueError(f"No other modes supported yet: {mode}")
-
+        
+        # override arguments from yaml config file with command line arguments (if provided)
+        for k, v in cfg.items():
+            if getattr(pre_args, k) is not None:
+                # print(f"Overriding config value for {k} with command line argument: {getattr(pre_args, k)}")
+                cfg[k] = getattr(pre_args, k)
+        
+        # print("cfg.items() is:", cfg.items())
+        
+        # gets the valid dict keys from the parser
         valid_dests = set()
         for action in parser._actions:
             valid_dests.add(action.dest)
 
-        print("Valid dests:", valid_dests)
+        # print("Valid dests:", valid_dests)
+
 
         kept = {}
         for k, v in cfg.items():
             if k in valid_dests:
                 kept[k] = v
 
-        print("Kept:", kept)
+        # print("Kept:", kept)
 
         parser.set_defaults(**kept)
 
---
+++ machine_learning/run_cnn.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from parsing_args import parse_args
+
+
+
+
+if __name__ == "__main__":
+
+    args = parse_args(None)
+    print("Parsed arguments:", args)
